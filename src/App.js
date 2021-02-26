04f914c52d56e1acf3454ea764b0a305367b21a5
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-02-25 19:06:15 -0500

    im really scared lol hahah everythings fine

--- src/App.js
+++ src/App.js
@@ -4,53 +4,53 @@ import Panel from "./components/Panel";
 import Nav from "./components/Nav";
 import Map from "./components/Map";
 import GameBox from "./components/Gamebox";
-import Directions from "./components/Compass";
+// import Directions from "./components/Compass";
 import Score from "./components/Score";
 
-
-import borderData from './data/border';
-import leafletPip from 'leaflet-pip';
+import borderData from "./data/border";
+import leafletPip from "leaflet-pip";
 import L from "leaflet";
 
-let startButton = getElementById("start-button")
+// let startButton = getElementById("start-button")
 function App() {
- 
   const [center, setCenter] = useState([43.88, -72.7317]);
-  const [zoom, setZoom] = useState(8)
-  
+  const [zoom, setZoom] = useState(8);
+
+  function gameStart(evt) {
+    let layers = 0;
+    let xMin = -73.42613118833583;
+    let xMax = -71.51022535353107;
+    let yMin = 42.730315121762715;
+    let yMax = 45.00541896831666;
+    let newX;
+    let newY;
+    while (layers !== 1) {
+      newX = xMin + Math.random() * (xMax - xMin);
+      newY = yMin + Math.random() * (yMax - yMin);
+      layers = leafletPip.pointInLayer(
+        [newX, newY],
+        L.geoJSON(borderData),
+        true
+      ).length;
+    }
+    setCenter([newY, newX]);
+     setZoom(18);
+    evt.target.style.display = false;
+  }
+
   return (
     <div>
-      <Map center={center} />
+      <Map center={center} zoom={zoom} />
       <Panel />
-      <GameBox />
+      {/* <GameBox /> */}
       <Nav />
-      <Directions />
+      {/* <Directions /> */}
       <Score />
-             <button
-             id = "start-button"
-        onClick = { (evt) => {
-          let layers = 0;
-          let xMin = -73.42613118833583;
-          let xMax = -71.51022535353107;
-          let yMin = 42.730315121762715;
-          let yMax = 45.00541896831666;
-          let newX;
-          let newY; 
-          while (layers !== 1) {
-          newX = xMin + Math.random() * (xMax - xMin);
-          newY = yMin + Math.random() * (yMax - yMin);
-          layers = (leafletPip.pointInLayer([newX, newY], L.geoJSON(borderData), true)).length
-          }
-          setCenter([newY, newX]);
-          startButton.style.display = none
-          
-        }}>
-      
-        Click Here
+      <button id="start-button" onClick={gameStart}>
+        Start
       </button>
     </div>
   );
 }
-
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -7,25 +7,34 @@ import {
 } from "react-leaflet";
 import borderData from "../data/border";
 import leafletPip from "leaflet-pip";
-import L from "leaflet";
-import {useState} from 'react';
+import L, { map } from "leaflet";
+import {useState, useEffect} from 'react';
 
 function Map(props) {
   let vtOutline = borderData.geometry.coordinates[0].map((coords) => [
     coords[1],
     coords[0],
   ]); 
+
+
+  console.log(props.zoom)
   return (
     <MapContainer
       center={props.center}
-      zoom={8}
+      zoom = {8}
       scrollWheelZoom={false}
       doubleClickZoom={false}
       zoomControl={false}
       touchZoom={false}
       style={{ height: "600px", width: "600px" }}
+    
     >
-      <ChangeView></ChangeView>
+
+      {/* <SetView location={location}> 
+
+
+      </SetView> */}
+      {/* <ChangeView></ChangeView> */}
       <TileLayer
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
         attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
