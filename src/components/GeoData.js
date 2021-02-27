1975d5695046244b563bd817ca2f55133cc97e59
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-02-27 17:36:41 -0500

    poop

--- src/components/GeoData.js
+++ src/components/GeoData.js
@@ -8,7 +8,8 @@ import Panel from './Panel'
 export default function GeoData(props) {
   const data = ""
   const [county, setCounty] = useState("");
-  const [city, setCity] = useState("");
+  // const [city, setCity] = useState("");
+  const [town, SetTown] = useState ("")
 
   useEffect(() => {
     if (!data && props.start === true) {
@@ -24,18 +25,31 @@ export default function GeoData(props) {
           console.log(jsonObj.address)
           //   setData(jsonObj);
           setCounty(jsonObj.address.county);
-          setCity(jsonObj.address.city);
-        
-          // console.log(jsonObj.address.county);
-          // console.log(jsonObj.address.city);
-          //
-        });
+          // setCity(jsonObj.address.city);
+        //   setTown({
+        //     town: jsonObj.address.village || jsonObj.address.hamlet || json.address.city || json.address.town
+        // });
     }
-  });
+  )};
 
   return (
     <div>
       <Panel start={props.start} county = {county} city = {city} droppedPin = {props.droppedPin} giveUp = {props.userGiveUp} />
     </div>
   );
-}
+  }
+
+
+
+
+// if (jsonObj.address.city) {
+//   town = jsonObj.address.city;
+// } else if (jsonObj.address.town) {
+//   town = jsonObj.address.town;
+// } else if (jsonObj.address.village) {
+//   town = jsonObj.address.village;
+// } else if (jsonObj.address.hamlet) {
+//   town = jsonObj.address.village;
+// }
+// console.log(town)
+// })
