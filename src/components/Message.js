2d1336b20156736c9eb739401e19953b8906d35f
meganzinka <meganzinka@gmail.com>
2021-03-04 09:35:52 -0500

    message exit works

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,26 +1,29 @@
 import React from "react";
+import {useState} from "react"
 
 const Message = (props) => {
-    function exitMessage (event) {
-        props.setDisplayMessage(false); 
-    }
+const [hidden, setHidden] = useState(false)
   if (props.displayMessage === "win") {
     return (
-      <div class="message-wrapper">
+      <div class="message-wrapper" style ={hidden? {display: 'none'} : {display: 'flex'}}>
         <div class="message">
           <h1> Congratulations, you won!</h1>
           <h3> Your score was a score!</h3>
-          <button onClick = {props.setDisplayMessage(false)}>Exit</button>
+          <button onClick = {function () {
+            setHidden(true)
+          }}>Exit</button>
         </div>
       </div>
     );
   } else if (props.displayMessage === "lose") {
     return (
-      <div class="message-wrapper">
+      <div class="message-wrapper" style ={hidden? {display: 'none'} : {display: 'flex'}}>
         <div class="message">
           <h1>Sucks to Suck!</h1>
           <h3>You lose 10 points.</h3>
-          <button onClick = {props.setDisplayMessage(false)}>Exit</button>
+          <button onClick = {function () {
+            setHidden(true)
+          }}>Exit</button>
         </div>
       </div>
     );
