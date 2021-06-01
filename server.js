cfd4ebaea254b3912118dd6ab13d3b4572f78e5d
meganzinka <meganzinka@gmail.com>
2021-06-01 11:42:05 -0400

    modify npm script commands

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const port = process.env.PORT || 5000;
 const staticDir = path.resolve(__dirname + "/build")
 //Server set-up
 app.use(express.static(staticDir));
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-// );
 
 //Port setup
 app.listen(port, () => {
